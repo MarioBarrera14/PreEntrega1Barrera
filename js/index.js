b4f75ed71e5154de97a44e64d3ae159213fac6f2
Javier.Mario <75219747+MarioBarrera14@users.noreply.github.com>
2023-02-01 23:52:15 -0300

    coder js

--- js/index.js
+++ js/index.js
@@ -9,16 +9,16 @@ function convertir(){
     // el valor ingresado lo llamamos con document.getElementById
     if(document.getElementById("uno").checked){
         resultado=valore*dolar;
-        alert ("EL cambio de peso a dolar es: $"+resultado.toFixed(2));
+        alert ("EL cambio de peso Argentino a dolar es: $"+resultado.toFixed(2));
     }else if(document.getElementById("dos").checked){
         resultado=valore*euro;
-        alert ("El cambio de peso a euro es: $"+resultado.toFixed(2));
+        alert ("El cambio de peso Argentino a euro es: $"+resultado.toFixed(2));
     }else if(document.getElementById("tres").checked){
         resultado=valore*pesoChileno;
-        alert ("El cambio de peso a peso chileno es: $"+resultado.toFixed(2));
+        alert ("El cambio de peso Argentino a peso chileno es: $"+resultado.toFixed(2));
     }else if(document.getElementById("cuatro").checked){
         resultado=valore*pReal;
-        alert ("El cambio de peso a peso chileno es: $"+resultado.toFixed(2));
+        alert ("El cambio de peso Argentino a Real es: $"+resultado.toFixed(2));
     }
     else{
         alert("Deberas completar los campos que necesites cotizar")
