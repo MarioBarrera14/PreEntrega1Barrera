dd470bdaca8a5ad1ace3f7d0e87e3fbd6a50d362
Javier.Mario <75219747+MarioBarrera14@users.noreply.github.com>
2023-02-01 23:35:44 -0300

    coder js

---
+++ js/index.js
@@ -0,0 +1,26 @@
+    let resultado=0;
+    let dolar=366.33;
+    let euro=273.99;
+    let pesoChileno=0.2388;
+    let pReal=43.7750;
+
+function convertir(){
+    let valore= parseInt(document.getElementById("valor").value);
+    // el valor ingresado lo llamamos con document.getElementById
+    if(document.getElementById("uno").checked){
+        resultado=valore*dolar;
+        alert ("EL cambio de peso a dolar es: $"+resultado.toFixed(2));
+    }else if(document.getElementById("dos").checked){
+        resultado=valore*euro;
+        alert ("El cambio de peso a euro es: $"+resultado.toFixed(2));
+    }else if(document.getElementById("tres").checked){
+        resultado=valore*pesoChileno;
+        alert ("El cambio de peso a peso chileno es: $"+resultado.toFixed(2));
+    }else if(document.getElementById("cuatro").checked){
+        resultado=valore*pReal;
+        alert ("El cambio de peso a peso chileno es: $"+resultado.toFixed(2));
+    }
+    else{
+        alert("Deberas completar los campos que necesites cotizar")
+    }
+}
